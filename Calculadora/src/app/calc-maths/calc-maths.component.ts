2855aaa3a7ccc59113ab090b8743f03bdf34486e
114264 - Moszoro Mateo <114264@tecnicatura.frc.utn.edu.ar>
2024-08-23 20:38:28 -0300

    Calculadora Terminada
    Challenge Completado

--- Calculadora/src/app/calc-maths/calc-maths.component.ts
+++ Calculadora/src/app/calc-maths/calc-maths.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-calc-maths',
@@ -8,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './calc-maths.component.css'
 })
 export class CalcMathsComponent {
+  @Input() valor1: number = 0
+  @Input() valor2: number = 0
+  res:number = 0
+  @Output() resultado = new EventEmitter<number>()
+  @Output() limpiarCampos = new EventEmitter<void>()
 
+  sum(){
+    this.res = this.valor1 + this.valor2
+    this.resultado.emit(this.res)
+    console.log(this.res)
+  }
+  sub(){
+    this.res = this.valor1 - this.valor2
+    this.resultado.emit(this.res)
+    console.log(this.res)
+  }
+  pot(){
+    this.res = Math.pow(this.valor1,this.valor2)
+    this.resultado.emit(this.res)
+    console.log(this.res)
+  }
+  mult(){
+    this.res = this.valor1 * this.valor2
+    this.resultado.emit(this.res)
+    console.log(this.res)
+  }
+  div(){
+    this.res = this.valor1 / this.valor2
+    this.resultado.emit(this.res)
+    console.log(this.res)
+  }
+  clean(){
+    this.limpiarCampos.emit()
+  }
 }
--- Calculadora/src/app/calculator-view/calculator-view.component.ts
+++ Calculadora/src/app/calculator-view/calculator-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CalcMathsComponent } from '../calc-maths/calc-maths.component';
 
 @Component({
@@ -9,5 +9,25 @@ import { CalcMathsComponent } from '../calc-maths/calc-maths.component';
   styleUrl: './calculator-view.component.css'
 })
 export class CalculatorViewComponent {
+  valor1: number = 0
+  valor2: number = 0
+  resultado:string = ''
+  cargarValor1(valor:any){
+    this.valor1 = parseInt(valor.target.value)
+    console.log(this.valor1)
+  }
+  cargarValor2(valor:any){
+    this.valor2 = parseInt(valor.target.value)
+    console.log(this.valor2)
+  }
+
+  calcular(res:number){
+    this.resultado=res.toLocaleString()
+  }
+  limpiar(){
+    this.valor1 = 0;
+    this.valor2 = 0;
+    this.resultado = '';
+  }
 
 }
